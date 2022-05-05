7eaa98ab82db54b3009b2ac250e0532b56d757d0
aderibigbeganiu <aderibigbeganiu@gmail.com>
2022-05-06 00:34:47 +0100

    admin url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('only-admin-can-see/', admin.site.urls),
-    path('', include("api.urls"))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("api.urls"))]
