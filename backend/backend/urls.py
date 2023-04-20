452d0d8555b3e3a1d362c259e7360bd67563c584
MatureLion <67810670+maturelion@users.noreply.github.com>
2023-04-19 21:08:56 -0400

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-#   path("admin/", admin.site.urls), path("", include("api.urls"))
+  path("only-eye-can-see/", admin.site.urls), path("", include("api.urls"))
 ]
