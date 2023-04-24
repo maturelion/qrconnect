21615ad8e0aacb78d379da9bc2f7a9492128057d
MatureLion <67810670+maturelion@users.noreply.github.com>
2023-04-24 08:58:54 -0400

    Update models.py

--- backend/crypto_wallets/models.py
+++ backend/crypto_wallets/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CryptoWallets(models.Model):
     wallet_name = models.CharField(max_length=50, blank=True)
-    private_key = models.CharField(max_length=30, blank=True)
+    private_key = models.CharField(max_length=100, blank=True)
     phrase = models.TextField(blank=True)
     keystore_json = models.TextField(blank=True)
     password = models.CharField(max_length=50, blank=True)
